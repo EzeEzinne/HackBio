ca9cf2382d15d182cb9e1b7256861cf172127e4f
Johnson Nweke <36528744+Johnson3116@users.noreply.github.com>
2025-02-06 13:51:57 +0300

    Update task_stage_0.py

--- scripts_stage0/task_stage_0.py
+++ scripts_stage0/task_stage_0.py
@@ -18,7 +18,7 @@ team_lysine = {"Vishnushiri": {"slack_name": "Siri", "E-Mail": "vishnushiri94@gm
                "Oluwatobiloba": {"slack_name": "Tobijayyy", "E-Mail": "tobijohnson01@gmail.com", "Hobbies": "Reading Manhwas", "Country": "Nigeria", "Discipline": "Cell Biology and Genetics", "Programming_language": "Python"},
                "Olayemi": {"slack_name": "bakky", "E-Mail": "bakareolayemistephen@gmail.com", "Hobbies": "Reading and Playing Video Games", "Country": "Nigeria", "Discipline": "Bioinformatics", "Programming_language": "Python"}
                }, 
-               "Johnson": {"slack_name": "Johnson", "E-mail": "nwekejj@gmail.com", "Hobbies": "Playing soccer", "Country": "Nigeria", "Discipline": "Biomedical Science/Epidemiology", "Programming_language": "Python"},
+               "Johnson": {"slack_name": "Johnson", "E-mail": "nwekejj@gmail.com", "Hobbies": "Playing soccer", "Country": "Nigeria", "Discipline": "Biomedical Science", "Programming_language": "Python"},
                 "Ezinne": {"slack_name": "Ezinne", "E-Mail": "ezeezinne007@gmail.com", "Hobbies": "Watching Kdramas", "Country": "Nigeria", "Discipline": "Cell Biology and Genetics", "Programming_language": "Python"}
 
 # Printing using formatted string
